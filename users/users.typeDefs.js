bec4f55ffd839921252017f339a01c35a7f81ce6
yoonhero <yoonhero06@naver.com>
2021-03-13 10:19:38 +0900

    1.2

--- users/users.typeDefs.js
+++ users/users.typeDefs.js
@@ -11,12 +11,6 @@ export default gql`
     updatedAt: String!
     bio: String
     avatar: String
-    photos: [Photo]
-    following: [User]
-    followers: [User]
-    totalFollowing: Int!
-    totalFollowers: Int!
-    isFollowing: Boolean!
     isMe: Boolean!
   }
 `;
